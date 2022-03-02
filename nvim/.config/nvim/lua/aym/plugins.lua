f79ef9a0a903f19c8894132dc8fcaf2eb0c60679
Ayhan Mesin <ayhanm90@gmx.de>
2022-03-02 20:55:34 +0100

    Add some comments to nvim plugins

--- nvim/.config/nvim/lua/aym/plugins.lua
+++ nvim/.config/nvim/lua/aym/plugins.lua
@@ -1,15 +1,23 @@
 return require('packer').startup(function()
     use 'wbthomason/packer.nvim'
 
+    -- Theme
     use 'dracula/vim'
 
+    -- Telescope
     use 'nvim-lua/plenary.nvim'
     use 'nvim-telescope/telescope.nvim'
 
+    -- Git
     use 'tpope/vim-fugitive'
+
+    -- Lsp
     use 'neovim/nvim-lspconfig'
+
+    -- Go
     use { 'fatih/vim-go', run = ':GoUpdateBinaries' }
 
+    -- File nav tree
     use {
         'kyazdani42/nvim-tree.lua',
         requires = {
@@ -18,14 +26,17 @@ return require('packer').startup(function()
         config = function() require'nvim-tree'.setup {} end
     }
 
+    -- Code completion
     use { 'hrsh7th/nvim-cmp' }
     use { 'hrsh7th/cmp-buffer' }
     use { 'hrsh7th/cmp-nvim-lsp' }
     use { 'hrsh7th/cmp-path' }
 
+    -- Snippets
     use { 'L3MON4D3/LuaSnip' }
     use { 'saadparwaiz1/cmp_luasnip' }
 
+    -- Treesitter
     use {
         'nvim-treesitter/nvim-treesitter',
         run = ':TSUpdate',
@@ -37,6 +48,7 @@ return require('packer').startup(function()
         }
     }
 
+    -- Status line
     use {
         'nvim-lualine/lualine.nvim',
         requires = { 'kyazdani42/nvim-web-devicons' }
