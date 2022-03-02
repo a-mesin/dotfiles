84f3a317b37670abb4055d39ef1a3e12cbae0787
Ayhan Mesin <ayhanm90@gmx.de>
2022-03-02 19:16:31 +0100

    Use dracula colors in nvim-ts-rainbow plugin

--- nvim/.config/nvim/lua/aym/treesitter.lua
+++ nvim/.config/nvim/lua/aym/treesitter.lua
@@ -1,25 +1,21 @@
 require'nvim-treesitter.configs'.setup {
-  -- One of "all", "maintained" (parsers with maintainers), or a list of languages
   ensure_installed = "maintained",
-
-  -- Install languages synchronously (only applied to `ensure_installed`)
   sync_install = false,
-
   highlight = {
-    -- `false` will disable the whole extension
     enable = true,
-
-    -- list of language that will be disabled
-    -- disable = { "c", "rust" },
-
-    -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
-    -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
-    -- Using this option may slow down your editor, and you may see some duplicate highlights.
-    -- Instead of true it can also be a list of languages
     additional_vim_regex_highlighting = false,
   },
   rainbow = {
       enable = true,
-      extended_mode = false
+      extended_mode = false,
+      colors = {
+          "#f1fa8c",
+          "#50fa7b",
+          "#bd93f9",
+          "#ff79c6",
+          "#ffb86c",
+          "#8be9fd",
+          "#ff5555"
+      }
   }
 }
