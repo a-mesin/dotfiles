92d0eba36947b38be3d8ac2cc91fe820c261b9bd
Ayhan Mesin <aym@cluetec.de>
2023-01-27 08:32:27 +0100

    add linting conf to gopls

--- nvim/.config/nvim/lua/aym/go.lua
+++ nvim/.config/nvim/lua/aym/go.lua
@@ -9,3 +9,7 @@ g.go_highlight_operators = 1
 g.go_fmt_autosave = 1
 g.go_fmt_command = "goimports"
 
+g.go_metalinter_autosave = 1
+g.go_metalinter_autosave_enabled = { 'vet', 'errcheck', 'staticcheck' }
+g.go_metalinter_command = 'golangci-lint'
+
