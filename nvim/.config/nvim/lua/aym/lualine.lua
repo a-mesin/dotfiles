2e7643ad9e2b33d79d63d9e387b3aca94584b3c9
Ayhan Mesin <ayhanm90@gmx.de>
2023-05-12 00:13:37 +0200

    nvim: use kanagawa theme for lualine

--- nvim/.config/nvim/lua/aym/lualine.lua
+++ nvim/.config/nvim/lua/aym/lualine.lua
@@ -1,57 +1,30 @@
-local colors = {
-  green  = '#50fa7b',
-  cyan   = '#8be9fd',
-  black  = '#282a36',
-  white  = '#c6c6c6',
-  red    = '#ff5189',
-  violet = '#bd93f9',
-  grey   = '#303030',
-}
-
-local bubbles_theme = {
-  normal = {
-    a = { fg = colors.black, bg = colors.violet },
-    b = { fg = colors.white, bg = colors.grey },
-    c = { fg = colors.black, bg = colors.black },
-  },
-
-  insert = { a = { fg = colors.black, bg = colors.green } },
-  visual = { a = { fg = colors.black, bg = colors.cyan } },
-  replace = { a = { fg = colors.black, bg = colors.red } },
-
-  inactive = {
-    a = { fg = colors.white, bg = colors.black },
-    b = { fg = colors.white, bg = colors.black },
-    c = { fg = colors.black, bg = colors.black },
-  },
-}
+local kanagawa = require("lualine.themes.kanagawa")
 
 require('lualine').setup {
-  options = {
-    theme = bubbles_theme,
-    component_separators = '|',
-    section_separators = { left = '', right = '' },
-  },
-  sections= {
-    lualine_a = {
-      { 'mode', separator = { left = '' }, right_padding = 2 },
+    options = {
+        theme = kanagawa,
+        component_separators = '|',
     },
-    lualine_b = { 'filename', 'branch' },
-    lualine_c = { 'fileformat' },
-    lualine_x = {},
-    lualine_y = { 'filetype', 'progress' },
-    lualine_z = {
-      { 'location', separator = { right = '' }, left_padding = 2 },
+    sections = {
+        lualine_a = {
+            { 'mode', right_padding = 2 },
+        },
+        lualine_b = { 'filename', 'branch' },
+        lualine_c = { 'fileformat' },
+        lualine_x = {},
+        lualine_y = { 'filetype', 'progress' },
+        lualine_z = {
+            { 'location', left_padding = 2 },
+        },
     },
-  },
-  inactive_sections = {
-    lualine_a = { 'filename' },
-    lualine_b = {},
-    lualine_c = {},
-    lualine_x = {},
-    lualine_y = {},
-    lualine_z = { 'location' },
-  },
-  tabline = {},
-  extensions = { 'fugitive', 'nvim-tree' },
-} 
+    inactive_sections = {
+        lualine_a = { 'filename' },
+        lualine_b = {},
+        lualine_c = {},
+        lualine_x = {},
+        lualine_y = {},
+        lualine_z = { 'location' },
+    },
+    tabline = {},
+    extensions = {},
+}
