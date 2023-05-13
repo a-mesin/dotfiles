d8b5985bba57f7f1d78c11cc1604e59db0a9320d
Ayhan Mesin <ayhanm90@gmx.de>
2023-05-14 00:09:17 +0200

    nvim: register lsp mappings through which-key

--- nvim/.config/nvim/lua/aym/lsp.lua
+++ nvim/.config/nvim/lua/aym/lsp.lua
@@ -1,3 +1,5 @@
+require("neodev").setup({})
+
 local lsp = require('lsp-zero')
 
 lsp.preset('recommended')
@@ -8,17 +10,17 @@ lsp.ensure_installed({
 
 local cmp = require('cmp')
 local cmp_mappings = lsp.defaults.cmp_mappings({
-  ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
-  ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
-  ['<C-y>'] = cmp.mapping.confirm({ select = true }),
-  ['<C-Space>'] = cmp.mapping.complete(),
+    ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
+    ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
+    ['<C-y>'] = cmp.mapping.confirm({ select = true }),
+    ['<C-Space>'] = cmp.mapping.complete(),
 })
 
 cmp_mappings['<Tab>'] = nil
 cmp_mappings['<S-Tab>'] = nil
 
 lsp.setup_nvim_cmp({
-  mapping = cmp_mappings
+    mapping = cmp_mappings
 })
 
 lsp.set_preferences({
@@ -31,21 +33,47 @@ lsp.set_preferences({
     }
 })
 
-lsp.on_attach(function(client, bufnr)
-  local opts = {buffer = bufnr, remap = false}
-  lsp.buffer_autoformat()
+lsp.on_attach(function(_, bufnr)
+    local opts = { buffer = bufnr, remap = false }
+    lsp.buffer_autoformat()
 
-  vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
-  vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
-  vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
-  vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
-  vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
-  vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
-  vim.keymap.set("n", "<leader>ca", function() vim.lsp.buf.code_action() end, opts)
-  vim.keymap.set("n", "<leader>rr", function() vim.lsp.buf.references() end, opts)
-  vim.keymap.set("n", "<leader>rn", function() vim.lsp.buf.rename() end, opts)
-  vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
+    local wk = require('which-key')
+    wk.register({
+        l = {
+            name = "LSP",
+            d = {
+                '<cmd>Telescope lsp_definitions<cr>',
+                'Go to definition'
+            },
+            r = {
+                '<cmd>Telescope lsp_references<cr>',
+                'References'
+            },
+            k = {
+                '<cmd>lua vim.lsp.buf.hover()<cr>',
+                'Hover'
+            },
+            wd = {
+                '<cmd>Telescope diagnostics<cr>',
+                'Diagnostics'
+            },
+            ws = {
+                '<cmd>Telescope lsp_dynamic_workspace_symbols<cr>',
+                'Symbols'
+            },
+            a = {
+                '<cmd>lua vim.lsp.buf.code_action()<cr>',
+                'Code Action'
+            },
+            rn = {
+                '<cmd> lua vim.lsp.buf.rename()<cr>',
+                'Rename'
+            }
+        }
+
+    }, { prefix = '<leader>', buffer = bufnr, remap = false })
+
+    vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
 end)
 
 lsp.setup()
-
