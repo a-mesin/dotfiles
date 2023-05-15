75836d140985c85eb5b553cb91fef42eb76f6b6d
Ayhan Mesin <ayhanm90@gmx.de>
2023-05-15 23:13:42 +0200

    nvim: add sql to treesitter & fix formatting

--- nvim/.config/nvim/lua/aym/treesitter.lua
+++ nvim/.config/nvim/lua/aym/treesitter.lua
@@ -1,22 +1,22 @@
-require'nvim-treesitter.configs'.setup {
-  ensure_installed = { "go", "typescript", "javascript", "lua", "vim" },
-  ignore_install = { "phpdoc" },
-  sync_install = false,
-  highlight = {
-    enable = true,
-    additional_vim_regex_highlighting = false,
-  },
-  rainbow = {
-      enable = true,
-      extended_mode = false,
-      colors = {
-          "#f1fa8c",
-          "#50fa7b",
-          "#bd93f9",
-          "#ff79c6",
-          "#ffb86c",
-          "#8be9fd",
-          "#ff5555"
-      }
-  }
+require 'nvim-treesitter.configs'.setup {
+    ensure_installed = { "go", "typescript", "javascript", "lua", "vim", "sql" },
+    ignore_install = { "phpdoc" },
+    sync_install = false,
+    highlight = {
+        enable = true,
+        additional_vim_regex_highlighting = false,
+    },
+    rainbow = {
+        enable = true,
+        extended_mode = false,
+        colors = {
+            "#f1fa8c",
+            "#50fa7b",
+            "#bd93f9",
+            "#ff79c6",
+            "#ffb86c",
+            "#8be9fd",
+            "#ff5555"
+        }
+    }
 }
