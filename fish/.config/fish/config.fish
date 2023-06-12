b0760571b8650f4a3174c6fbce4d89fa72a971ad
Ayhan Mesin <ayhanm90@gmx.de>
2023-06-12 23:05:55 +0200

    fish: add kubectl aliases with plugin

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -2,4 +2,7 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
+test -f ~/.kubectl_aliases.fish && source \
+     (cat ~/.kubectl_aliases.fish | sed -r 's/(kubectl.*) --watch/watch \1/g' | psub)
+
 starship init fish | source
