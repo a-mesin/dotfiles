4d014a5b4daeb712bd0939094d60d0e21433c12f
Ayhan Mesin <ayhanm90@gmx.de>
2023-12-06 22:11:32 +0100

    nvim: add svelte to treesitter

--- nvim/.config/nvim/lua/aym/treesitter.lua
+++ nvim/.config/nvim/lua/aym/treesitter.lua
@@ -1,5 +1,5 @@
 require 'nvim-treesitter.configs'.setup {
-    ensure_installed = { "go", "typescript", "javascript", "lua", "vim", "sql", "templ" },
+    ensure_installed = { "go", "typescript", "javascript", "lua", "vim", "sql", "templ", "svelte" },
     ignore_install = { "phpdoc" },
     sync_install = false,
     highlight = {
