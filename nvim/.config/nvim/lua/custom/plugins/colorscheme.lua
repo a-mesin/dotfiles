ef22ddd3fe91509dda301c77e415c24eff86a05d
Ayhan Mesin <aym@cluetec.de>
2024-05-24 07:37:29 +0200

    nvim: adjust colorscheme background

--- nvim/.config/nvim/lua/custom/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/custom/plugins/colorscheme.lua
@@ -2,6 +2,8 @@ return {
 	"mcchrish/zenbones.nvim",
 	dependencies = { "rktjmp/lush.nvim" },
 	config = function()
+		vim.g.zenbones = { darkness = "stark" }
+
 		vim.cmd.colorscheme("zenbones")
 	end,
 }
