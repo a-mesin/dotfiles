0a4ce22fa418f877b25a1a153b4b1acce6b3daf1
Ayhan Mesin <aym@cluetec.de>
2024-05-29 08:05:08 +0200

    nvim: switch to no-fiesta-clown theme

--- nvim/.config/nvim/lua/custom/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/custom/plugins/colorscheme.lua
@@ -1,9 +1,13 @@
 return {
-	"mcchrish/zenbones.nvim",
-	dependencies = { "rktjmp/lush.nvim" },
+	-- "mcchrish/zenbones.nvim",
+	-- dependencies = { "rktjmp/lush.nvim" },
+	-- config = function()
+	-- 	vim.g.zenbones = { darkness = "stark" }
+	--
+	-- 	vim.cmd.colorscheme("zenbones")
+	-- end,
+	"aktersnurra/no-clown-fiesta.nvim",
 	config = function()
-		vim.g.zenbones = { darkness = "stark" }
-
-		vim.cmd.colorscheme("zenbones")
+		vim.cmd.colorscheme("no-clown-fiesta")
 	end,
 }
