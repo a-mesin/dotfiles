280f5d687043bcdc14df2d493e90ba13b839f833
Ayhan Mesin <aym@cluetec.de>
2024-08-22 12:59:44 +0200

    nvim: add todo plugin

---
+++ nvim/.config/nvim/lua/custom/plugins/todo.lua
@@ -0,0 +1,23 @@
+return {
+	"folke/todo-comments.nvim",
+	dependencies = { "nvim-lua/plenary.nvim" },
+	opts = {
+		-- your configuration comes here
+		-- or leave it empty to use the default settings
+		-- refer to the configuration section below
+	},
+	keys = {
+		{
+			"<leader>tt",
+			"<cmd>TodoTelescope<cr>",
+		},
+		{
+			"<leader>tf",
+			"<cmd>TodoQuickFix<cr>",
+		},
+		{
+			"<leader>ttr",
+			"<cmd>TodoTrouble<cr>",
+		},
+	},
+}
