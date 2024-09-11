259f04c6b0f979c484662523f0b4622eb49cc1b0
Ayhan Mesin <ayhanm90@gmx.de>
2024-09-11 22:01:59 +0200

    nvim: switch to cyperdream

--- nvim/.config/nvim/lua/custom/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/custom/plugins/colorscheme.lua
@@ -4,18 +4,31 @@
 -- 		vim.cmd("colorscheme no-clown-fiesta")
 -- 	end,
 -- 	--
--- }
 
+-- }
+-- return {
+-- 	"catppuccin/nvim",
+-- 	name = "catppuccin",
+-- 	lazy = false,
+-- 	priority = 1000,
+-- 	config = function()
+-- 		require("catppuccin").setup({
+-- 			transparent_background = true,
+-- 		})
+--
+-- 		vim.api.nvim_command("colorscheme catppuccin-mocha")
+-- 	end,
+-- }
 return {
-	"catppuccin/nvim",
-	name = "catppuccin",
+	"scottmckendry/cyberdream.nvim",
 	lazy = false,
 	priority = 1000,
 	config = function()
-		require("catppuccin").setup({
-			transparent_background = true,
+		require("cyberdream").setup({
+			transparent = true,
+			borderless_telescope = false,
 		})
 
-		vim.api.nvim_command("colorscheme catppuccin-mocha")
+		vim.cmd("colorscheme cyberdream")
 	end,
 }
