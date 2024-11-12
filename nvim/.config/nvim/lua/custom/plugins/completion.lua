4711814fbb1f115eb4a7414388a0697b0ecef150
Ayhan Mesin <a.mesin@cluetec.de>
2024-11-12 08:02:11 +0100

    nvim: enable signature_help for completion

--- nvim/.config/nvim/lua/custom/plugins/completion.lua
+++ nvim/.config/nvim/lua/custom/plugins/completion.lua
@@ -32,9 +32,9 @@ return {
 		nerd_font_variant = "mono",
 
 		-- experimental auto-brackets support
-		-- accept = { auto_brackets = { enabled = true } }
+		accept = { auto_brackets = { enabled = true } },
 
 		-- experimental signature help support
-		-- trigger = { signature_help = { enabled = true } }
+		trigger = { signature_help = { enabled = true } },
 	},
 }
