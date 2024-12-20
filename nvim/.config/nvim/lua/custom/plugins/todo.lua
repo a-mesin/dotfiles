fd0e51d9830dcdf23e7342bb3acbc409f95b50bb
Ayhan Mesin <a.mesin@cluetec.de>
2024-12-20 11:04:13 +0100

    nvim: disable lazy loading for todo.nvim

--- nvim/.config/nvim/lua/custom/plugins/todo.lua
+++ nvim/.config/nvim/lua/custom/plugins/todo.lua
@@ -1,6 +1,7 @@
 return {
 	"folke/todo-comments.nvim",
 	dependencies = { "nvim-lua/plenary.nvim" },
+	lazy = false,
 	opts = {
 		-- your configuration comes here
 		-- or leave it empty to use the default settings
