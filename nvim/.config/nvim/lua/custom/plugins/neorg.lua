030581733d3033c8b28bbaf326dc886e143f62da
Ayhan Mesin <a.mesin@cluetec.de>
2024-12-20 11:03:55 +0100

    nvim: add neorg plugin

---
+++ nvim/.config/nvim/lua/custom/plugins/neorg.lua
@@ -0,0 +1,25 @@
+return {
+	"nvim-neorg/neorg",
+	lazy = false, -- Disable lazy loading as some `lazy.nvim` distributions set `lazy = true` by default
+	version = "*", -- Pin Neorg to the latest stable release
+	config = function()
+		require("neorg").setup({
+			load = {
+				["core.defaults"] = {},
+				["core.concealer"] = {
+					config = { -- We added a `config` table!
+						icon_preset = "diamond", -- And we set our option here.
+					},
+				},
+				["core.dirman"] = {
+					config = {
+						workspaces = {
+							notes = "~/notes",
+						},
+						default_workspace = "notes",
+					},
+				},
+			},
+		})
+	end,
+}
