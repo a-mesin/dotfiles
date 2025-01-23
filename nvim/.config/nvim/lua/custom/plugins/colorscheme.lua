30ef109579dbbcbb3b28180a854badb9cf16ea5d
Ayhan Mesin <ayhanm90@gmx.de>
2025-01-23 22:33:34 +0100

    nvim: add transparent background to carbonfox

--- nvim/.config/nvim/lua/custom/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/custom/plugins/colorscheme.lua
@@ -1,41 +1,16 @@
 ---@diagnostic disable: missing-fields
 
 -- return {
--- 	"jesseleite/nvim-noirbuddy",
--- 	dependencies = {
--- 		{ "tjdevries/colorbuddy.nvim" },
--- 	},
--- 	lazy = false,
--- 	priority = 1000,
--- 	config = function()
--- 		require("noirbuddy").setup({
--- 			preset = "oxide",
--- 			colors = {
--- 				secondary = "#f8ff18",
--- 			},
--- 		})
---
--- 		local Group = require("colorbuddy").Group
--- 		local colors = require("colorbuddy").colors
---
--- 		Group.new("@keyword", colors.secondary)
--- 		Group.new("@variable", colors.noir_2)
--- 	end,
--- }
-
--- return {
--- 	"AlexvZyl/nordic.nvim",
--- 	lazy = false,
--- 	priority = 1000,
--- 	config = function()
--- 		require("nordic").load()
--- 	end,
--- }
-
--- return {
 -- 	"EdenEast/nightfox.nvim",
 -- 	priority = 1000,
 -- 	config = function()
+-- 		-- Default options
+-- 		require("nightfox").setup({
+-- 			options = {
+-- 				transparent = true, -- Disable setting background
+-- 			},
+-- 		})
+--
 -- 		vim.cmd("colorscheme carbonfox")
 -- 	end,
 -- }
