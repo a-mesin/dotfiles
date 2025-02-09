610509113869a87429d7e9fd78bd4b0ae45adeb3
Ayhan Mesin <ayhanm90@gmx.de>
2025-02-09 23:49:51 +0100

    add cyberdream & jellybeans themes

--- nvim/.config/nvim/lua/custom/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/custom/plugins/colorscheme.lua
@@ -1,32 +1,40 @@
 ---@diagnostic disable: missing-fields
 
 -- return {
--- 	"EdenEast/nightfox.nvim",
+-- 	"slugbyte/lackluster.nvim",
+-- 	lazy = false,
 -- 	priority = 1000,
--- 	config = function()
--- 		-- Default options
--- 		require("nightfox").setup({
--- 			options = {
--- 				transparent = true, -- Disable setting background
+-- 	init = function()
+-- 		local lackluster = require("lackluster")
+-- 		lackluster.setup({
+-- 			tweak_background = {
+-- 				normal = "none",
+-- 				popup = "none",
 -- 			},
 -- 		})
---
--- 		vim.cmd("colorscheme carbonfox")
+-- 		vim.cmd.colorscheme("lackluster-night")
 -- 	end,
 -- }
 --
 return {
-	"slugbyte/lackluster.nvim",
-	lazy = false,
+	"wtfox/jellybeans.nvim",
 	priority = 1000,
-	init = function()
-		local lackluster = require("lackluster")
-		lackluster.setup({
-			tweak_background = {
-				normal = "none",
-				popup = "none",
-			},
+	config = function()
+		require("jellybeans").setup({
+			transparent = true,
 		})
-		vim.cmd.colorscheme("lackluster-night")
+		vim.cmd.colorscheme("jellybeans")
 	end,
 }
+--
+-- return {
+-- 	"scottmckendry/cyberdream.nvim",
+-- 	lazy = false,
+-- 	priority = 1000,
+-- 	config = function()
+-- 		require("cyberdream").setup({
+-- 			transparent = true,
+-- 		})
+-- 		vim.cmd.colorscheme("cyberdream")
+-- 	end,
+-- }
