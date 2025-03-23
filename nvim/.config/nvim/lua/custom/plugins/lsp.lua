ffcf250469599595f331ff59cd92dd2876c8a702
Ayhan Mesin <a.mesin@cluetec.de>
2025-03-23 22:52:14 +0100

    nvim: add vue lsp support

--- nvim/.config/nvim/lua/custom/plugins/lsp.lua
+++ nvim/.config/nvim/lua/custom/plugins/lsp.lua
@@ -38,11 +38,68 @@ return {
 					"typescript",
 					"typescriptreact",
 					"svelte",
+					"vue",
+				},
+				init_options = {
+					plugins = {
+						{
+							name = "@vue/typescript-plugin",
+							location = vim.fn.stdpath("data")
+								.. "/mason/packages/vue-language-server/node_modules/@vue/language-server",
+							languages = { "vue" },
+						},
+					},
+				},
+				settings = {
+					typescript = {
+						tsserver = {
+							useSyntaxServer = false,
+						},
+						inlayHints = {
+							includeInlayParameterNameHints = "all",
+							includeInlayParameterNameHintsWhenArgumentMatchesName = true,
+							includeInlayFunctionParameterTypeHints = true,
+							includeInlayVariableTypeHints = true,
+							includeInlayVariableTypeHintsWhenTypeMatchesName = true,
+							includeInlayPropertyDeclarationTypeHints = true,
+							includeInlayFunctionLikeReturnTypeHints = true,
+							includeInlayEnumMemberValueHints = true,
+						},
+					},
 				},
 			},
 			rust_analyzer = {},
 			svelte = {},
 			lua_ls = {},
+			volar = {
+				init_options = {
+					vue = {
+						hybridMode = false,
+					},
+				},
+				settings = {
+					typescript = {
+						inlayHints = {
+							enumMemberValues = {
+								enabled = true,
+							},
+							functionLikeReturnTypes = {
+								enabled = true,
+							},
+							propertyDeclarationTypes = {
+								enabled = true,
+							},
+							parameterTypes = {
+								enabled = true,
+								suppressWhenArgumentMatchesName = true,
+							},
+							variableTypes = {
+								enabled = true,
+							},
+						},
+					},
+				},
+			},
 		}
 
 		-- Add other tools you want to make sure are installed
