0fc35f981af6f26c059fce112d352c91a8bf7793
Ayhan Mesin <a.mesin@cluetec.de>
2025-06-04 11:42:27 +0200

    nvim: remove deprecated volar lsp config

--- nvim/.config/nvim/lua/custom/plugins/lsp.lua
+++ nvim/.config/nvim/lua/custom/plugins/lsp.lua
@@ -63,35 +63,6 @@ return {
 			rust_analyzer = {},
 			svelte = {},
 			lua_ls = {},
-			volar = {
-				init_options = {
-					vue = {
-						hybridMode = false,
-					},
-				},
-				settings = {
-					typescript = {
-						inlayHints = {
-							enumMemberValues = {
-								enabled = true,
-							},
-							functionLikeReturnTypes = {
-								enabled = true,
-							},
-							propertyDeclarationTypes = {
-								enabled = true,
-							},
-							parameterTypes = {
-								enabled = true,
-								suppressWhenArgumentMatchesName = true,
-							},
-							variableTypes = {
-								enabled = true,
-							},
-						},
-					},
-				},
-			},
 		}
 
 		-- Add other tools you want to make sure are installed
