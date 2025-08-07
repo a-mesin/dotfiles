cddb72b05dded792c5ffdc170052b1810dca9058
Ayhan Mesin <ayhan.mesin@possehl-digital.services>
2025-08-07 22:32:11 +0200

    nvim: use vague theme

--- nvim/.config/nvim/lua/custom/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/custom/plugins/colorscheme.lua
@@ -1,18 +1,17 @@
 ---@diagnostic disable: missing-fields
-
+---
 -- return {
 -- 	"slugbyte/lackluster.nvim",
 -- 	lazy = false,
 -- 	priority = 1000,
 -- 	init = function()
--- 		local lackluster = require("lackluster")
--- 		lackluster.setup({
+-- 		require("lackluster").setup({
 -- 			tweak_background = {
 -- 				normal = "none",
 -- 				popup = "none",
 -- 			},
 -- 		})
--- 		vim.cmd.colorscheme("lackluster-night")
+-- 		vim.cmd.colorscheme("lackluster")
 -- 	end,
 -- }
 --
@@ -38,15 +37,50 @@
 -- 		vim.cmd.colorscheme("cyberdream")
 -- 	end,
 -- }
+--
+-- return {
+-- 	"no-clown-fiesta/no-clown-fiesta.nvim",
+-- 	lazy = false,
+-- 	priority = 1000,
+-- 	config = function()
+-- 		require("no-clown-fiesta").setup({
+-- 			transparent = true,
+-- 		})
+-- 		vim.cmd.colorscheme("no-clown-fiesta")
+-- 	end,
+-- }
+
+--
+
 return {
 	{
-		"Skardyy/makurai-nvim",
+		"vague2k/vague.nvim",
+		lazy = false, -- make sure we load this during startup if it is your main colorscheme
+		priority = 1000, -- make sure to load this before all the other plugins
 		config = function()
-			require("makurai").setup({
-				transparent = true, -- removes the bg color
+			require("vague").setup({
+				transparent = true,
 			})
-
-			vim.cmd.colorscheme("makurai_less")
+			vim.cmd.colorscheme("vague")
 		end,
 	},
+	-- {
+	-- 	"wnkz/monoglow.nvim",
+	-- 	config = function()
+	-- 		require("monoglow").setup({
+	-- 			-- Change the "glow" color
+	-- 			on_colors = function(colors)
+	-- 				colors.glow = "#18c784"
+	-- 			end,
+	-- 		})
+	--
+	-- 		vim.cmd.colorscheme("monoglow")
+	-- 	end,
+	-- },
+	-- {
+	-- 	"ficcdaf/ashen.nvim",
+	-- 	config = function()
+	-- 		vim.cmd.colorscheme("ashen")
+	-- 	end,
+	-- },
 }
